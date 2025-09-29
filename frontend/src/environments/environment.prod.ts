82f9e7c4d7b597f621c90cecd54605dd1c7c2484
Parth Kawatra <parthkawatra@hotmail.com>
2025-09-29 16:02:13 +0530

    change minor fix

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://notes-app-full-stack-example.vercel.app/'  // replace with your deployed backend URL
+  apiUrl: 'https://notes-app-full-stack-example.vercel.app'  // replace with your deployed backend URL
 };
